2625dc8cc10f85bb09d1e3835a3ed2b20767a6c7
Francisco Uriel  Bazán <franbazan014@gmail.com>
2023-09-26 21:20:13 -0300

    Agrego funcionalidades a la página de Alta

--- src/contexts/ProductoContext.jsx
+++ src/contexts/ProductoContext.jsx
@@ -1,9 +1,9 @@
 import { createContext, useEffect, useState } from "react"
-import { get } from "../utils/http"
+import { del, get, post, put } from "../utils/http"
 
 const ProductoContext = createContext()
 
-const url = 'http://localhost:8080/productos'
+const url = import.meta.env.VITE_URL
 const ProductoProvider = ( { children } ) => {
     const [productos, setProductos] = useState(null)
 
@@ -21,7 +21,38 @@ const ProductoProvider = ( { children } ) => {
         }
     }
 
-    const data = {productos}
+    const crearProductoContext = async (productoNuevo) => {
+        try {
+            const productoCreado = await post(url, productoNuevo)
+            setProductos([...productos, productoCreado])
+        } catch (error) {
+            console.error('Algo pasó en el crearProductoContext', error)
+        }
+    }
+
+    const actualizarProductoContext = async (productoAEditar) => {
+        try {
+            const productoeEditado = await put(url, productoAEditar.id, productoAEditar)
+            console.log(productoeEditado)
+            const nuevaDB = productos.map( producto => producto.id === productoeEditado.id ? productoeEditado : producto)
+            setProductos(nuevaDB)
+        } catch (error) {
+            console.error('Ocurrió un problema en actualizarProductoContext', error)
+        }
+    }
+
+    const eliminarProductoContext = async (id) => {
+        try {
+            const productoEliminado = await del(url, id)
+            console.log(productoEliminado)
+            let nuevaDB = productos.filter(producto => producto.id !== id)
+            setProductos(nuevaDB)
+        } catch (error) {
+            console.error('Algo pasó en el eliminarProductoContext', error)
+        }
+    }
+
+    const data = {productos, crearProductoContext, eliminarProductoContext, actualizarProductoContext}
 
     return <ProductoContext.Provider value={data}>{children}</ProductoContext.Provider>
 }
--- src/utils/http.js
+++ src/utils/http.js
@@ -16,3 +16,65 @@ export const get = async (url) => {
         console.error('ERROR GET', error)
     }
 }
+
+export const post = async (url, dato) => {
+    try {
+        
+        const config = {
+            method: 'POST',
+            headers: { 'content-type': 'application/json'},
+            body: JSON.stringify(dato)
+        }
+
+        const respuesta = await fetch(url, config)
+        if ( !respuesta.ok ) {
+            throw new Error(`Error POST ${respuesta.status} ${respuesta.statusText}`)
+        }
+        const ProductoCreado = await respuesta.json()
+        return ProductoCreado
+
+    } catch (error) {
+        console.error('ERROR POST', error)
+    }
+}
+
+export const put = async (url, id, dato) => {
+    try {
+        const config = {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json'},
+            body: JSON.stringify(dato)
+        }
+
+        const urlFull = url + id
+        const respuesta = await fetch(urlFull, config)
+        if ( !respuesta.ok ) {
+            throw new Error(`Ocurrió un problema ${respuesta.status} ${respuesta.statusText}`)
+        }
+        const productoEditado = await respuesta.json()
+        return productoEditado
+        
+    } catch (error) {
+        
+    }
+}
+
+export const del = async (url, id) => {
+    try {
+        const config = {
+            method: 'DELETE'
+        }
+        const urlFull = url + id
+        console.log(urlFull)
+        const respuesta = await fetch(urlFull, config)
+        if ( !respuesta.ok ) {
+            throw new Error(`Ocurrió un problema ${respuesta.status} ${respuesta.statusText}`)
+        }
+        const productoEliminado = await respuesta.json()
+        return productoEliminado
+
+    } catch (error) {
+        console.error('ERROR DELETE', error)
+    }
+
+}
